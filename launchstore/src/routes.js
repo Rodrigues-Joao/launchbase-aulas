9f3ed20939d1c583ea952722ec5e7beb5043e679
João Victor Rodrigues <joao15.vi.rodrigues@gmail.com>
2021-03-10 22:24:01 -0300

    implementação - controller do produto - pagina create - fields para páginas create e edit*

--- launchstore/src/routes.js
+++ launchstore/src/routes.js
@@ -1,9 +1,14 @@
 const express = require('express')
+const ProdcutController = require('./app/controllers/product-controller')
+
+
 const routes = express.Router()
 routes.get('/', (req, res) => {
-
     return res.render("layout.njk")
-
+})
+routes.get('/products/create', ProdcutController.create)
+routes.get('/ads/create', (req, res) => {
+    return res.redirect('/products/create')
 })
 
 
